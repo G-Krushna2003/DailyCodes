d16a50af636eff37e753c08d5cdf5d99696e0655
krushna <krishgomsale@gmail.com>
2024-09-14 16:38:46 +0530

    Todays code modified

--- Accenture/code1.java
+++ Accenture/code1.java
@@ -2,7 +2,7 @@ package Accenture;
 public class code1 {
     public static void main(String[] args) {
         
-        int arr[]={1,2,3,4,5,6,7,8,9};
+        int arr[]={1,2,3,4,5,6,7,8};
         int num=5;
 
         if(arr.length<=9){
